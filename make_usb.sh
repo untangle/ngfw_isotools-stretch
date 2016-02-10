f2056b4f08a20847af843d18d6baa5b046a80d25
Sebastien Delafond <sdelafond@gmail.com>
2016-02-10 19:08:47 +0000

    [isotools] USB installer (Comment: #12795)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/isotools-jessie@42423 8ebf8b4e-06f3-0310-961f-89f74618be54

--- make_usb.sh
+++ make_usb.sh
@@ -11,13 +11,23 @@ ISO_IMG=$(ls -t /tmp/iso-images/UNTANGLE*iso | head -1)
 IMG_NAME=$(basename $IMG)
 IMG_NOZIP=$(echo $IMG_NAME | perl -pe 's/\.gz$//')
 MOUNT_POINT=/mnt
-SYSLINUX_LOOP=$MOUNT_POINT/syslinux.cfg
+ISO_MOUNT_POINT=/mnt2
 
 gunzip -c $IMG >| $IMG_NOZIP
 umount -f $MOUNT_POINT || true
 mount -o loop $IMG_NOZIP $MOUNT_POINT
 df -h
-cp -f $ISOTOOLS_DIR/cd-root/isolinux/* $MOUNT_POINT/
+
+# grab isolinux conf directly from the ISO image
+mkdir -p $ISO_MOUNT_POINT
+mount -o loop $ISO_IMG $ISO_MOUNT_POINT
+cp $ISO_MOUNT_POINT/isolinux/* $MOUNT_POINT/
+umount $ISO_MOUNT_POINT
+
+# tweak conf a bit
+mv $MOUNT_POINT/isolinux.cfg $MOUNT_POINT/syslinux.cfg
+perl -i -pe 's|vmlinuz|linux| ; s|/install.\w+/|| ; s|gtk/initrd|initrdg|' $MOUNT_POINT/{gtk,txt}.cfg
+
 cp -r $ISOTOOLS_DIR/cd-root/images $ISOTOOLS_DIR/tmp/extras/simple-cdd $MOUNT_POINT
 cp $ISO_IMG $MOUNT_POINT/$(echo $(basename $ISO_IMG) | perl -pe 's|:||g')
 umount $MOUNT_POINT
