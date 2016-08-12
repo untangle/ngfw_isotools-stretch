060f3f6460cef27ed246e853030e3d66ca52d751
Sebastien Delafond <sdelafond@gmail.com>
2016-08-12 21:50:13 +0000

    [isotools] Debug
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/isotools-jessie@44029 8ebf8b4e-06f3-0310-961f-89f74618be54

--- package-server-proxy.sh
+++ package-server-proxy.sh
@@ -3,11 +3,13 @@
 # FIMXE: does it get any nastier than this ??
 # make that a CGI on package-server, for fucksake...
 
+env
+
 REMOTE_USER="buildbot@package-server"
 SSH_OPTIONS="-o StrictHostKeyChecking=no"
 TMP_DIR=/tmp/package-server-proxy_$$
 
-ssh $SSH_OPTIONS $REMOTE_USER "rm -fr $TMP_DIR ; mkdir $TMP_DIR"
+ssh -v $SSH_OPTIONS $REMOTE_USER "rm -fr $TMP_DIR ; mkdir $TMP_DIR"
 scp $SSH_OPTIONS -q *sh $REMOTE_USER:$TMP_DIR
 ssh $SSH_OPTIONS $REMOTE_USER "cd $TMP_DIR ; $@"
 rc=$?
