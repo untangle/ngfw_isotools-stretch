04d286bf2f08a5d443bdcdcd8f2123d5250dc4ef
Sébastien Delafond <sdelafond@gmail.com>
2016-11-25 15:07:19 +0100

    [asus-ac88u] A squashfs that's too short fails the boot process, dynamically create a long and fake one instead

--- firmware/asus-ac88u/firmware.sh
+++ firmware/asus-ac88u/firmware.sh
@@ -7,6 +7,12 @@ BIN_DIR=${CURRENT_DIR}/binary
 
 TMP_IMAGE=${CURRENT_DIR}/tmp/tmp.img
 
+# dynamically create a 28M dummy squashfs filled with zeros
+dd if=/dev/zero of=$3 bs=1MB count=28
+
+# assemble the TRX file
 ${BIN_DIR}/trx -m 40000000 -o $TMP_IMAGE $2 -a 131072 $3
+
+# run through trx_vendor to compute a correct TRX checksum
 ${BIN_DIR}/trx_vendor -i $TMP_IMAGE -r RT-AC88U,3.0.0.4,380,760,$1
 
