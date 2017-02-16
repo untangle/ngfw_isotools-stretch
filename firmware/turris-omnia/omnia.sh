ced50f01745b41e5ebe101f0d1fb1db5056da6e2
Sébastien Delafond <sdelafond@gmail.com>
2017-02-16 08:25:23 +0100

    [omnia] Modify post-imaging script to reduce size of final download

--- firmware/turris-omnia/omnia.sh
+++ firmware/turris-omnia/omnia.sh
@@ -3,16 +3,14 @@
 set -e
 set -x
 
-# constants
-CURRENT_DIR=$(dirname $0)
-
 # CL args
 IMAGE=$1
 
 # include vendor-specific configuration
-VENDOR_DIR=${CURRENT_DIR}/../${NAME}
-source ${VENDOR_DIR}/image.conf
+source image.conf
 
+# variables
+NAME=$(basename $(readlink -f .))
 TARBALL_FILE=$(mktemp /tmp/tmp.${NAME}-tarball.XXXXX.tar.gz)
 MNT_DIR=$(mktemp -d /tmp/tmp.${NAME}-img.XXXXX)
 
@@ -23,10 +21,27 @@ mount $LOOP_DEVICE $MNT_DIR
 # tar up content
 tar -C $MNT_DIR -czvf $TARBALL_FILE .
 
-# wipe out IMG content
-rm -fr ${MNT_DIR:-/foo}/*
+# umount
+umount $MNT_DIR
+losetup -d $LOOP_DEVICE
 
-# move tarball back in
+# re-create smaller disk image
+dd if=/dev/zero of=$IMAGE bs=10M count=65
+fdisk $IMAGE <<EOF
+n
+p
+1
+
+
+w
+EOF
+
+# format and mount it
+losetup $LOOP_DEVICE  $IMAGE -o $(( 512 * 2048 ))
+mkfs.ext4 $LOOP_DEVICE
+mount $LOOP_DEVICE $MNT_DIR
+
+# move tarball in
 mv $TARBALL_FILE ${MNT_DIR}/omnia-medkit-last.tar.gz
 
 # umount & cleanup
