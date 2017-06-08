e234c79cc3f156295a0c4d692913eff4d71fed8d
Sébastien Delafond <sdelafond@gmail.com>
2017-06-08 18:33:13 +0200

    [iso] Don't touch /var/www/public/jessie/dists whatsoever

--- create-di-links.sh
+++ create-di-links.sh
@@ -9,10 +9,10 @@ STABLE_DISTRIBUTION=testing
 DISTRIBUTION_DIR=/var/www/public/$REPOSITORY/dists
 
 for d in $STABLE_DISTRIBUTION jessie jessie-updates ; do
-  sudo rm -f $DISTRIBUTION_DIR/$d
+#  sudo rm -f $DISTRIBUTION_DIR/$d
 #  sudo ln -sf $DISTRIBUTION_DIR/$DISTRIBUTION $DISTRIBUTION_DIR/$d
 done
 
-cd $DISTRIBUTION_DIR/$DISTRIBUTION
+#cd $DISTRIBUTION_DIR/$DISTRIBUTION
 #sudo rm -f updates
 #sudo ln -sf ./ ./updates
