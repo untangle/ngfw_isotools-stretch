8feb9199a521375b0d6ed9de80d358b03a235a09
Sébastien Delafond <sdelafond@gmail.com>
2017-06-09 18:55:19 +0200

    [iso] Only setup jessie & testing links on package-server

--- create-di-links.sh
+++ create-di-links.sh
@@ -8,12 +8,7 @@ DISTRIBUTION=$2
 STABLE_DISTRIBUTION=testing
 DISTRIBUTION_DIR=/var/www/public/$REPOSITORY/dists
 
-for d in $STABLE_DISTRIBUTION jessie jessie-updates ; do
-  :
-#  sudo rm -f $DISTRIBUTION_DIR/$d
-#  sudo ln -sf $DISTRIBUTION_DIR/$DISTRIBUTION $DISTRIBUTION_DIR/$d
+for d in $STABLE_DISTRIBUTION jessie ; do
+ sudo rm -f $DISTRIBUTION_DIR/$d
+ sudo ln -sf $DISTRIBUTION_DIR/$DISTRIBUTION $DISTRIBUTION_DIR/$d
 done
-
-#cd $DISTRIBUTION_DIR/$DISTRIBUTION
-#sudo rm -f updates
-#sudo ln -sf ./ ./updates
