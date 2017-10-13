89f63ad040a0480df41caa3bc7dd6a0429c8a1fd
Sébastien Delafond <sdelafond@gmail.com>
2017-10-13 10:26:12 +0200

    Try to populate simple-cdd/ from git checkout

--- make_usb.sh
+++ make_usb.sh
@@ -8,6 +8,7 @@ ISOTOOLS_DIR=$(dirname $0)
 BASE_IMG=$1
 ISO_IMG=$2
 OUT_IMG=$3
+FLAVOR=$4
 
 IMG_NAME=$(basename $BASE_IMG)
 MOUNT_POINT=$(mktemp -d /tmp/tmp.untangle-usb.XXXXX)
@@ -29,7 +30,19 @@ rm -r $ISO_MOUNT_POINT
 mv $MOUNT_POINT/isolinux.cfg $MOUNT_POINT/syslinux.cfg
 perl -i -pe 's|vmlinuz|linux| ; s|/install.\w+/|| ; s|gtk/initrd|initrdg|' $MOUNT_POINT/{gtk,txt}.cfg
 
-cp -r $ISOTOOLS_DIR/cd-root/images $ISOTOOLS_DIR/tmp/extras/simple-cdd $MOUNT_POINT
+cp -r $ISOTOOLS_DIR/cd-root/images $MOUNT_POINT
+mkdir $MOUNT_POINT/simple-cdd
+cp $ISOTOOLS_DIR/profiles/{default.conf,default.downloads,default.excludes,default.packages,default.preseed,default.udebs,expert.preseed,${FLAVOR}.downloads,${FLAVOR}.packages,${FLAVOR}.preseed} $MOUNT_POINT/simple-cdd/
+cat > $MOUNT_POINT/simple-cdd/simple-cdd.templates <<EOF
+Template: simple-cdd/profiles
+Type: multiselect
+Choices: untangle, ${FLAVOR}, expert
+Default: 
+Description: Select profiles
+ profiles are a simple description of common system types.
+ select all that apply.
+EOF
+
 cp $ISO_IMG $MOUNT_POINT/$(echo $(basename $ISO_IMG) | perl -pe 's|:||g')
 umount $MOUNT_POINT
 rm -r $MOUNT_POINT
