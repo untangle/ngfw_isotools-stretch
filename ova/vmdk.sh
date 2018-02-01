92359b8e5209e92af4f1f84095c715768c1a4a1e
Sébastien Delafond <sdelafond@gmail.com>
2018-02-01 05:39:20 +0100

    [ova] Use ut-qemu-mkimage from u-dev-kernel to build base qcow2 image

--- ova/vmdk.sh
+++ ova/vmdk.sh
@@ -13,80 +13,25 @@ DISTRIBUTION=$2
 ARCH=$3
 VMDK=$4
 FLAVOR=$5
-shift 5
-EXTRA_PACKAGES=$@
 
 ## main
 QCOW2=${VMDK/vmdk/qcow2}
-BASE_TMP_DIR="/tmp/tmp.vmdk-chroot-${FLAVOR}-${DISTRIBUTION}"
-CHROOT_DIR=$(mktemp -d ${BASE_TMP_DIR}.XXXXX)
 TMP_VMDK="/tmp/${FLAVOR}.vmdk"
+FLAVOR_PACKAGES_FILE="${CURRENT_DIR}/../profiles/${FLAVOR}.packages"
 
-# clean up if something went wrong during previous run, but make
-# sure we do it in the right order, or we'll mess up the host system
-# since those were bind mounts
-if mount | grep -q ${BASE_TMP_DIR} ; then
-  for dir in ${BASE_TMP_DIR}.* ; do
-    for pfs in sys proc dev/pts dev ; do
-      umount ${dir}/$pfs || true
-    done
-  done
-fi
+# create comma-separated list of extra packages
+extraPackages=$(grep -h -vE '^#' ${CURRENT_DIR}/extra-packages.txt $FLAVOR_PACKAGES_FILE | xargs)
+extraPackages=${extraPackages// /,}
 
-# NBD support
-rmmod nbd || true
-modprobe nbd max_part=32
-
-case $FLAVOR in
-  adtran) NUM=3 ;;
-  apc) NUM=6 ;;
-  untangle) NUM=9 ;;
-  *) NUM=0 ;;
-esac
-while [[ -e /var/lock/qemu-nbd-nbd${NUM} ]] ; do
-  NUM=$(( NUM + 1 ))
-done
-NBD_DEV="/dev/nbd${NUM}"
-
-# create qcow2
-vmdebootstrap --arch $ARCH \
-              --distribution $REPOSITORY \
-              --mirror http://deb.debian.org/debian \
-              --size 80GB \
-              --verbose \
-              --root-password passwd \
-              --sparse \
-	      --grub \
-              --image $QCOW2
-
-# attach and mount
-qemu-nbd -d ${NBD_DEV} || true
-qemu-nbd -c ${NBD_DEV} ${QCOW2}
-sleep 3
-mount ${NBD_DEV}p1 ${CHROOT_DIR}
-
-# mount required PFS
-for pfs in dev dev/pts proc sys ; do
-  mount --bind /$pfs ${CHROOT_DIR}/$pfs
-done
-
-# setup Untangle in chroot
-cp ${CURRENT_DIR}/${SETUP_SCRIPT} ${CHROOT_DIR}/tmp/
-extraPackagesFile=${CURRENT_DIR}/extra-packages.txt
-if [ -f $extraPackagesFile ] ; then
-  cp $extraPackagesFile ${CHROOT_DIR}/tmp/
-fi
-chroot ${CHROOT_DIR} /tmp/${SETUP_SCRIPT} ${REPOSITORY} ${DISTRIBUTION} ${ARCH} ${NBD_DEV} ${EXTRA_PACKAGES}
-
-# umount PFS
-for pfs in sys proc dev/pts dev ; do
-  umount -l ${CHROOT_DIR}/$pfs || true
-done
-
-# free ressources, and cleanup
-umount ${CHROOT_DIR}
-qemu-nbd -d ${NBD_DEV}
-rm -fr ${CHROOT_DIR}
+# create qcow2 by using latest untangle-development-kernel's ut-mkimage
+apt-get update
+apt-get install --yes untangle-development-kernel
+# FIXME: stable is usually the branch; as a 1st step, we prolly want
+# to case $DISTRIBUTION and infer suite name.
+# This will however not work if we maintain 3 branches at the same
+# (which happened with master=14.0 + release-13.2 + release-13.1 for a
+# while)
+ut-qemu-mkimage -u -r $REPOSITORY -d stable -p $extraPackages -f $QCOW2
 
 # convert back to an ESX-compatible VMDK
 qemu-img convert -O vmdk -o subformat=streamOptimized ${QCOW2} ${TMP_VMDK}
